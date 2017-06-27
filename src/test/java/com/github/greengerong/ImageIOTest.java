63765a19a53cfc6f83133f4662fb44b59814a086
polang <grzx2210@163.com>
2017-06-27 22:20:14 +0800

    ImageIO

--- src/test/java/com/github/greengerong/ImageIOTest.java
+++ src/test/java/com/github/greengerong/ImageIOTest.java
@@ -3,9 +3,9 @@ package com.github.greengerong;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.net.URL;
 import javax.imageio.ImageIO;
 
+import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
 /******************************************
@@ -20,18 +20,29 @@ public class ImageIOTest {
     @Test
     public void should_merge_image() throws Exception {
 
-        final String mapUri = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Topographic_map_example.png/440px-Topographic_map_example.png";
-        final BufferedImage mapImg = ImageIO.read(new URL(mapUri));
-        System.out.println(this.getClass().getResource("fav_checked.png") + "=======");
-        final BufferedImage markImg = ImageIO.read(this.getClass().getResource("fav_checked.png"));
-
+        long start = System.currentTimeMillis();
+        final BufferedImage mapImg = ImageIO.read(new File("./src/test/resources/map-example.png"));
+        final BufferedImage markImg = ImageIO.read(new File("./src/test/resources/mark.jpeg"));
 
         BufferedImage combined = new BufferedImage(mapImg.getWidth(), mapImg.getWidth(), BufferedImage.TYPE_INT_ARGB);
 
         Graphics g = combined.getGraphics();
         g.drawImage(mapImg, 0, 0, null);
-        g.drawImage(markImg, 0, 0, null);
+        g.setColor(Color.RED);
 
-        ImageIO.write(mapImg, "PNG", new File("./target", "map-combined.png"));
+        ImmutableList
+                .of(new Point(100, 50), new Point(100, 200), new Point(310, 140))
+                .stream()
+                .forEach(point -> {
+                    g.drawImage(markImg, point.getX(), point.getY(), 30, 30, null);
+                    g.drawString(String.format("%d,%d", point.getX(), point.getY()), point.getX(), point.getY() - 10);
+                });
+
+
+        ImageIO.write(combined, "PNG", new File("./target", "map-combined.png"));
+
+        final long end = System.currentTimeMillis();
+        System.out.println("performance: " + (end - start));
     }
 }
+
